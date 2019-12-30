c81152abb621e4e50cf6501c673a9c0f3bcda4de
charleselumeze <charleselumeze@gmail.com>
2019-12-30 02:11:20 +0000

    Add project files.

---
+++ IoTBot.CoreIoT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IoTBot.CoreIoT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
